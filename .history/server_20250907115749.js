103b3b41c84918c6fe8fbcec5192e339512004ff
Rohit Rana <rkrana6631@gmail.com>
2025-09-09 13:07:41 +0530

    update

--- .history/server_20250907115749.js
+++ .history/server_20250907115749.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5300;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
